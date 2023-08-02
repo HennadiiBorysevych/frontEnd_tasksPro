35baaac6f20e60f14b148dd8d7a04efbb80e8ef7
Hennadii Borysevych <genuch3@gmail.com>
2023-08-02 18:30:08 +0200

    feat: chaanges

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="">
+        <BrowserRouter basename="/frontEnd_tasksPro">
           <App />
         </BrowserRouter>
       </PersistGate>
