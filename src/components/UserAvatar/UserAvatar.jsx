915d07361f01788e4c9346e51e2e5ca465fb44bb
Роман <muzzlestore@gmail.com>
2023-08-04 01:02:43 +0300

    add ProfilePopUp

--- src/components/UserAvatar/UserAvatar.jsx
+++ src/components/UserAvatar/UserAvatar.jsx
@@ -17,8 +17,8 @@ const UserAvatar = ({ avatar, width = '68px', height = '68px', margin }) => {
 
 UserAvatar.propTypes = {
   avatar: PropTypes.string,
-  width: PropTypes.string.isRequired,
-  height: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
   margin: PropTypes.string,
 };
 
--- src/components/profilePopUp/ProfilePopUp.jsx
+++ src/components/profilePopUp/ProfilePopUp.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ProfilePopUp = () => {
-  return <div>ProfilePopUp</div>;
+import PopUpLayout from 'components/PopUp/PopUpLayout/PopUpLayout';
+
+import { UserAvatar } from 'components';
+
+import { Container, AvatarWrap, AddButtonWrap } from './ProfilePopUp.styled';
+
+const ProfilePopUp = ({
+  user,
+  handleAddAvatar,
+  handleEditProfile,
+  handleModalClose,
+}) => {
+  return (
+    <Container>
+      <PopUpLayout title="Edit profile" handleClose={handleModalClose}>
+        <AvatarWrap>
+          <UserAvatar avatar={null} />
+          <AddButtonWrap></AddButtonWrap>
+        </AvatarWrap>
+        <input value={user?.name} />
+        <input value={user?.email} />
+        <input value={user?.password} />
+        <button onClick={handleEditProfile}>Send</button>
+      </PopUpLayout>
+    </Container>
+  );
+};
+
+ProfilePopUp.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }),
+  handleAddAvatar: PropTypes.func.isRequired,
+  handleEditProfile: PropTypes.func.isRequired,
+  handleModalClose: PropTypes.func.isRequired,
 };
 
 export default ProfilePopUp;
