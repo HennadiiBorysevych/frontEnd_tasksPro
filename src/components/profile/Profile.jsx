c15fbda2e0f9823b45c4540259456af4106725d8
Роман <muzzlestore@gmail.com>
2023-08-03 23:34:35 +0300

    initial commit

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Profile = () => {
-  return <div>Profile</div>;
+import UserAvatar from 'components/UserAvatar/UserAvatar';
+
+import { Container, UserName } from './Profile.styled';
+
+const Profile = ({ user }) => {
+  return (
+    <Container>
+      <UserName>{user?.name ?? 'No User'}</UserName>
+      <UserAvatar avatar={user?.avatar} width="32px" height="32px" />
+    </Container>
+  );
 };
 
 export default Profile;
