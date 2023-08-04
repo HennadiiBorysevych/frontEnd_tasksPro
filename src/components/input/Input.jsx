c22f2315f42019a4c0b1060a33902c9230f5af6e
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2023-08-04 14:03:06 +0300

    added styles to the component input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  InputStyled,
+  TextareaStyled,
+  PasswordWrapperIcon,
+} from './Input.styled';
+import { SvgIcon } from 'components';
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({
+  multiline,
+  height,
+  type,
+  name,
+  placeholder,
+  onChange,
+  background,
+  ...rest
+}) => {
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleTogglePassword = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword);
+  };
+
+  return (
+    <>
+      {multiline ? (
+        <TextareaStyled
+          height={height}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          background={background}
+          {...rest}
+        />
+      ) : (
+        <InputStyled
+          type={showPassword ? 'text' : type}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          background={background}
+          {...rest}
+        />
+      )}
+      {type === 'password' && (
+        <PasswordWrapperIcon onClick={handleTogglePassword}>
+          <SvgIcon
+            svgName={showPassword ? 'icon-eye' : 'icon-eye-close'}
+            width="18"
+            height="18"
+            fill="#ffffff"
+          />
+        </PasswordWrapperIcon>
+      )}
+    </>
+  );
 };
 
 export default Input;
