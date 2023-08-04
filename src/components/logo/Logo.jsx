a78dc1bf38e93e7371f4ada32f24ea1de68b7f5e
SamoilenkoIvan <113337841+SamoilenkoIvan@users.noreply.github.com>
2023-08-05 00:14:03 +0300

    feat:createlogo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import sprite from 'assets/images/sprite.svg';
 
-const Logo = () => {
-  return <div>Logo</div>;
+  const Logo = () => {
+    const location = useLocation();
+    const isRegistrationRoute = location.pathname === ' api/auth/register ';
+    const logoSize = isRegistrationRoute ? { width: 18, height: 24 } : { width: 12, height: 16 };
+
+  return (
+    <div>
+      <svg width={logoSize.width} height={logoSize.height}>
+        <use href={sprite + '#icon-icon'} fill="none" stroke="#000"></use>
+      </svg>
+    </div>
+  );
 };
 
 export default Logo;
