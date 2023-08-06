9297e9ec0c37beb4c968beb921cc586d6c3e1e38
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-08-06 08:47:14 +0300

    fix:yup-schemas

--- src/validationSchemas/registerSchema.js
+++ src/validationSchemas/registerSchema.js
@@ -1,20 +1,34 @@
 import * as Yup from 'yup';
 
 const registerSchema = Yup.object().shape({
-  name: Yup.string().min(2, 'Too Short!').max(32, 'Too Long!').trim(),
-  email: Yup.string()
-    .email('Invalid email')
+  name: Yup.string()
+    .min(2, 'Name must be at least 2 characters')
+    .max(32, 'Name cannot be longer than 32 characters')
     .matches(
-      '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$',
-      'Email may contain only letters, apostrophe, dash and spaces and numbers. For example 9.WaFFe!s@mail.com'
+      /^[a-zA-Z0-9\s\-_!@#$%^&*()+=?<>{}[\]\\/]+$/,
+      'Invalid characters in name'
     )
-    .required('Required')
-    .trim(),
+    .required('Name is required'),
+  email: Yup.string()
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      'Invalid email format'
+    )
+    .transform((value, originalValue) =>
+      originalValue ? originalValue.replace(/\s+/g, '') : value
+    )
+    .required('Email is required'),
   password: Yup.string()
-    .required('Required')
-    .min(8, 'Too Short!')
-    .max(64, 'Too Long!')
-    .trim(),
+    .min(8, 'Password must be at least 8 characters')
+    .max(64, "Password can't be longer than 64 characters")
+    .matches(
+      /^[a-zA-Z0-9!@#$%^&*()\-_=+?<>{}[\]\\/]+$/,
+      'Invalid characters in password'
+    )
+    .transform((value, originalValue) =>
+      originalValue ? originalValue.replace(/\s+/g, '') : value
+    )
+    .required('Password is required'),
 });
 
 export default registerSchema;
