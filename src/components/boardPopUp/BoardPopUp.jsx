357d30998c65d72a93f9a86cac871bdf997384ed
Роман <muzzlestore@gmail.com>
2023-08-07 23:00:31 +0300

    add primary BoardPopUp markup

--- src/components/boardPopUp/BoardPopUp.jsx
+++ src/components/boardPopUp/BoardPopUp.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const BoardPopUp = () => {
-  return <div>BoardPopUp</div>;
+import {
+  PopUpLayout,
+  BoardSettings,
+  ButtonPlus,
+  PrimaryButton,
+  Input,
+} from 'components';
+
+import { Container } from './BoardPopUp.styled';
+
+const BoardPopUp = ({ onClose }) => {
+  return (
+    <Container>
+      <PopUpLayout title="New board" handleClose={onClose}>
+        <Input style={{ marginBottom: '14px' }} placeholder="Title" />
+        <BoardSettings />
+        <PrimaryButton style={{ marginTop: '16px' }}>
+          <ButtonPlus />
+          Create
+        </PrimaryButton>
+      </PopUpLayout>
+    </Container>
+  );
+};
+
+BoardPopUp.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
 export default BoardPopUp;
