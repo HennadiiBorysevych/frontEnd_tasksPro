58a1ffd7463c7800a10eb930aaa3bac357a9643d
Hennadii Borysevych <genuch3@gmail.com>
2023-08-12 15:16:22 +0200

    feat:sidebar for skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
       <GlobalStyles />
 
       <Suspense>
-        {true ? (
+        {isFetchingCurrent ? (
           <SkeletonLoader page="/home/" />
         ) : (
           <Routes>
