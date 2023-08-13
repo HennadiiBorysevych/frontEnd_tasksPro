1a28d1ab411e20b28625e13edb4d0f9645288c14
Oleg <sirov@ukr.net>
2023-08-13 20:03:30 +0300

    fix cardSlice  columnSlice

--- src/redux/tasks/cardSlice.js
+++ src/redux/tasks/cardSlice.js
@@ -85,13 +85,13 @@ const tasksSlice = createSlice({
           createdAt,
           updatedAt,
           ...rest
-        } = action.payload.result;
+        } = action.payload.data;
         state.items.push({
           id,
           order,
           ...rest,
         });
-        console.log(`${action.payload.result.title} added to your tasks`);
+        console.log(`${action.payload.data.title} added to your tasks`);
       })
       .addCase(deleteTask.fulfilled, (state, action) => {
         state.isLoading = false;
@@ -114,12 +114,7 @@ const tasksSlice = createSlice({
       .addCase(updateTask.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        const {
-          _id: id,
-          createdAt,
-          updatedAt,
-          ...rest
-        } = action.payload.result;
+        const { _id: id, createdAt, updatedAt, ...rest } = action.payload.data;
         const index = state.items.findIndex(task => task.id === id);
         if (index !== -1) {
           state.items[index] = {
