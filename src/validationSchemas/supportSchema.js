0bd460791d232b4a606364ccd0eb5a85480f2b3c
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-08-14 07:30:47 +0300

    add: supportSchema

--- src/validationSchemas/supportSchema.js
+++ src/validationSchemas/supportSchema.js
@@ -2,13 +2,13 @@ import * as Yup from 'yup';
 
 const supportSchema = Yup.object().shape({
   email: Yup.string()
-    .required()
+    .required('Email is required')
     .matches(
       '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$',
       'Email may contain only letters, apostrophe, dash and spaces and numbers. For example 9.WaFFe!s@mail.com'
     )
     .trim(),
-  comment: Yup.string().required(),
+  comment: Yup.string().required('Comment is required'),
 });
 
 export default supportSchema;
