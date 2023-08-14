7115cc15bddb6f09e766c35d87252a8c2bc341aa
Роман <muzzlestore@gmail.com>
2023-08-15 00:08:33 +0300

    add modal close

--- src/hooks/useSupport.js
+++ src/hooks/useSupport.js
@@ -2,11 +2,12 @@ import { useDispatch } from 'react-redux';
 
 import { authOperations } from '../redux/auth';
 
-const useSupport = () => {
+const useSupport = onClose => {
   const dispatch = useDispatch();
 
   const handleSupportSubmit = values => {
     dispatch(authOperations.updateUserHelp(values));
+    onClose();
   };
   return { handleSupportSubmit };
 };
