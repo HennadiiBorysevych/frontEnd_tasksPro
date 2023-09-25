5a825eaee48ca615cb5a7cc0205aff6348128996
GregTerekhov <113180004+GregTerekhov@users.noreply.github.com>
2023-09-25 15:39:37 +0300

    refactor: boardHead logic connect with useBoard, create workspaceModel in constants, temporary fix AuthPage, create babel-plugin rules

---
+++ src/constants/workspaceInitialData.js
@@ -0,0 +1,22 @@
+export const boardModel = {
+  title: '',
+  icon: 'icon-Project',
+  background: '',
+  isActive: false,
+};
+
+export const columnModel = {
+  title: '',
+};
+
+export const cardModel = {
+  title: '',
+  description: '',
+  priority: 'Without',
+  deadline: '',
+};
+
+export const supportModel = {
+  email: '',
+  comment: '',
+};
